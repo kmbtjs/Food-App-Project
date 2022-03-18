45bcf9a239c01eda420b0e0ff913d415f01519a2
Kamer Berat Topal <kamertopalxp@gmail.com>
2022-03-18 17:31:42 +0300

    Some refactoring using partials on views

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,15 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+const burger = document.querySelector('#burger')
+const menu = document.querySelector('#menu')
+
+burger.addEventListener('click', () => {
+    if (menu.classList.contains('hidden')) {
+    menu.classList.remove('hidden')
+    }
+    else {
+    menu.classList.add('hidden');
+    }
+})
