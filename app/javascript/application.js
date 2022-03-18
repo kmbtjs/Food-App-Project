0d303e65016a3187feaaf76d3fb55bc3ae7f5cd7
Kamer Berat Topal <kamertopalxp@gmail.com>
2022-03-18 17:38:24 +0300

    Javascript Refactoring

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,15 +1,3 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-
-const burger = document.querySelector('#burger')
-const menu = document.querySelector('#menu')
-
-burger.addEventListener('click', () => {
-    if (menu.classList.contains('hidden')) {
-    menu.classList.remove('hidden')
-    }
-    else {
-    menu.classList.add('hidden');
-    }
-})
